487b327a846a4205753ce8e6ee11bbceb9859bbc
dominikwosiek <dominikwosiek@gmail.com>
2017-03-24 18:37:15 +0100

    Update hetman.py

--- Lab2/hetman.py
+++ Lab2/hetman.py
@@ -1,69 +1,96 @@
+print("Maximize")
 f = True
-n= int(raw_input())
-print('Maximize')
-for x in range(1, n+ 1):
-    for y in range(1, n+1):
+n = 10                                                  # wielkosc danych wejsciowych
+for x in range(ord('a'), ord('a') + n):
+    for y in range(1, n + 1):
         if f:
             f = False
         else:
-            print '+',
-        print 'a'+ str(x)+'_' + str(y),
-     
-print('')
-print('Subject to')
+            print('+', end='')
+        print(chr(x) + str(y), end='')
+print()
+print()
+print("Subject To")
+print()
 
-for x in range (1,n+1):
-    f = True
-    for y in range (1, n+1):
+for x in range(ord('a'), ord('a') + n):                 #wypisuje kolumny
+    f = True;
+    for y in range(1, n + 1):
         if f:
-            f = False;
+            f = False
         else:
-            print '+',
-        print 'a'+ str(x)+'_' + str(y),
-    print('<= 1')
+            print('+', end='')
+        print(chr(x) + str(y), end='')
+    print("<=1")
 
-for y in range (1,n+1):
+print()
+
+for y in range(1, n + 1):                           #wypisuje wiersze
     f = True
-    for x in range (1, n+1):
+    for x in range(ord('a'), ord('a') + n):
         if f:
-            f = False;
+            f = False
         else:
-            print '+',
-        print 'a'+ str(x)+'_' + str(y),
-    print('<= 1')
+            print('+', end='')
+        print(chr(x) + str(y), end='')
+    print("<=1")
 
-for s in range (3, 2 * n):
-    f = True
-    for x in range (1, n + 1):
-        y = s - x
-        if y < 1 or y > n:
-            continue
+print()
+
+for x in range(0, n-2):                                                   #wypisuje przekątne w kierunku w dół w prawo
+    for y in range (0, x+2):
         if f:
-            f = False;
+            f = False
         else:
-            print '+',
-        print 'a' + str(x) + '_' + str(y),
-    print('<= 1')
+            print('+', end='')
+        print(chr(ord('a')+y) + str(n-1-x+y), end='')
+    print("<=1")
 
-for s in range (-n+2, n-1):
+for x in range(0, n-1):                                              
     f = True
-    for x in range (1, n + 1):
-         y=s+x
-         if y < 1 or y > n:
-             continue
-         if f:
-             f= False;
-         else:
-             print '+',
-         print 'a'+ str(x) + '_' + str(y),
-    print('<= 1')    
- 
-print('Bounds') 
-for x in range (1,n+1):
-    for y in range (1,n+1):
-        print ('1 >= ' + 'a' + str(x) + '_' + str(y)+ ' >= 0')
-print('Generals')
-for x in range (1,n+1):
-    for y in range (1,n+1):
-        print('a' + str(x) + '_' + str(y))
-print ('End')
+    for y in range(0, n-x):
+        if f:
+            f = False
+        else:
+            print('+', end='')
+        print(chr(ord('a') + x + y ) + str(1+y), end='')
+    print("<=1")
+print()
+print()
+print()
+
+
+for x in range(0, n-2):                                         #wypisuje przekątne w kierunku w górę w prawo                                                  
+    for y in range (0, x+2):
+        if f:
+            f = False
+        else:
+            print('+', end='')
+        print(chr(ord('a')+y) + str(2+x-y), end='')
+    print("<=1")
+
+
+for x in range(0, n-1):
+    f = True
+    for y in range(0, n-x):
+        if f:
+            f = False
+        else:
+            print('+', end='')
+        print(chr(ord('a') + x + y) + str(n-y), end='')
+    print("<=1")
+print()
+
+print("Bounds")
+print()
+for x in range(ord('a'), ord('a') + n):
+    for y in range(1, n + 1):
+        print("0<=" + chr(x) + str(y) + "<=1", end=' ')
+
+print()
+print("Generals")
+for x in range(ord('a'), ord('a') + n):
+    for y in range(1, n + 1):
+        print(chr(x) + str(y), end=' ')
+print()
+print("End")
